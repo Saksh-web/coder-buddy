ed8f3fc244c6fa36b0870af7262c81b8a027f1e1
saksh sharma <saksh@sakshs-MacBook-Air.local>
2026-03-31 11:29:33 +0530

    added nodemon for auto server restart

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+console.log("file started");
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
