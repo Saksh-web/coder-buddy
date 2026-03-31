1ea55fcfef3ebe99f0eab2e44eff07e60db61ae3
saksh sharma <saksh@sakshs-MacBook-Air.local>
2026-03-31 16:28:59 +0530

    Added register page and EJS setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
 const express = require("express");
-
 const app = express();
+const path = require("path");
+const userRegister = require("./routes/register");
+
+// middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set EJS as view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 console.log("file started");
+
+
+
 
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
+app.use("/register",userRegister)
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
